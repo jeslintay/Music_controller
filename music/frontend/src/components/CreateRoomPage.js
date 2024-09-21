23f67dfc605342da9b278fe3fb1da15ced7f0e55
jeslintay <jeslin.tay.2020@sis.smu.edu.sg>
2024-09-21 16:20:21 +0800

    edited create page

--- music/frontend/src/components/CreateRoomPage.js
+++ music/frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,38 @@
 import React , {Component} from 'react';
+import Button from '@mui/material/Button';
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2;
     constructor(props){
         super(props);
     }
     render() {
-        return <p>This is the create room page</p>;
+        return (<Grid container spacing={1}>
+           <Grid item xs={12} align="center">
+            <Typography component='h4' variant='h4'>
+                Create a room
+            </Typography>
+           </Grid>
+           <Grid item xs={12} align="center">
+            <FormControl component='fieldset'>
+                <FormHelperText>
+                    <div align='center'>
+                        Guest Control of Playback State
+                    </div>
+                </FormHelperText>
+            </FormControl>
+           </Grid>
+        </Grid>);
     }
 }
 
